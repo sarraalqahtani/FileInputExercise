415b3c4942c6ce506d26212d192cc890abf410aa
Alqahtani <sarra-alqahtani@wfu.edu>
2020-09-07 09:57:10 -0400

    Initial commit

---
+++ src/FileInput.java
@@ -0,0 +1,10 @@
+public class FileInput {
+
+    public static void main(String[] args) {
+        //1) open moviesDataset.csv
+        //2) read each row and print it to the screen
+        //3) add exception handling when needed
+
+    }
+
+}
